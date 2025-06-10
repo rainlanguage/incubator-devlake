f891a93fe4871fecc30f9fac07955d3b12a838cf
rouzwelt <rouzweltt@gmail.com>
2025-06-10 22:53:17 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        pkgs = pkgs;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.go pkgs.gopls ];
+          shellHook = ''
+            export PATH=${pkgs.go}/bin:$PATH
+          '';
+        };
+      });
+}
